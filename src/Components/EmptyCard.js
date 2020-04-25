fbeaa4510237db9e7233403340f60884d84da7ef
Gabriel Gutierrez <35079758+Gabriel6go8d@users.noreply.github.com>
2020-04-26 00:45:35 +0200

    d

--- src/Components/EmptyCard.js
+++ src/Components/EmptyCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function EmptyCard(prop) {
     return (
-        <div className='mt-5 col-12 col-md-6 col-xl-3 m-0 p-0 testtt'>
+        <div className='mt-5 col-12 col-md-6 col-xl-4 m-0 p-0 testtt'>
             <div className='row m-0 p-0 justify-content-center'>
                 <div className='emptyCard' onClick={prop.add}>
                 </div>
